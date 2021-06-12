d2a2c6914a95cade11ac95c151206ccf67ecf6b1
alalloush <alhusein.alloush@gmail.com>
2021-06-12 17:39:45 +0200

    Documentation
    Added documentation for JS file and cleaned up unneeded code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-// Script to attach navbar to Burger Menu on smaller devices and toggles is-active
-// @since 10.6.2021
-// @author @bradcomp @wicky-info
-// source https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-// source https://gist.github.com/wicky-info/3ef6dfef7fe667ecb39df293ac6b9e5c
-//
-// Description: Code was adapted and set to run on page load. It will add an event listener on the burger menu and toggle the menu. 
-// It also takes the items from the dataset which is linked to the navbar items (navbarMenu) and enables them as well.
-
-
+/*
+Burger Menu Script
+Script to attach navbar to Burger Menu on smaller displays and toggles is-active
+---
+@since      10.6.2021
+@author     @bradcomp @wicky-info
+@source     https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+@source     https://gist.github.com/wicky-info/3ef6dfef7fe667ecb39df293ac6b9e5c
+---
+Description: Code was adapted and set to run on page load. It will add an event listener on the burger menu and toggle the menu. 
+It also takes the items from the dataset which is linked to the navbar items (navbarMenu) and enables them as well.
+*/
 window.onload=function() {
     var burger = document.querySelector('.burger');
     var menu = document.querySelector('#'+burger.dataset.target);
@@ -16,14 +18,17 @@ window.onload=function() {
         menu.classList.toggle('is-active');
     });
 }
-
-//This toggles pop-up modal on off status. It is called from the Impressum and the close button for it.
-function toggleModal(){
-    var modal = document.querySelector('.modal');
-    modal.classList.toggle('is-active');
-}
   
-//Language Switcher
+/*
+Language Changer Script
+Script to switch language between English and German
+---
+@since 12.6.2021
+@author @alalloush
+---
+Description: Code was adapted and set to run on page load. It will add an event listener on the burger menu and toggle the menu. 
+It also takes the items from the dataset which is linked to the navbar items (navbarMenu) and enables them as well.
+*/
 function switchLang(){
 
     Array.from(document.querySelectorAll('[lang="de"]'))
@@ -33,10 +38,21 @@ function switchLang(){
     .forEach(el =>el.classList.toggle("is-hidden"))
 }
 
-//Jump to top of page
 
-function jumper(location){
-    var element = document.getElementById(location);
+
+/*
+Smooth Scroll Script
+Script to smoothly jump to any passed ID tag
+---
+@since      12.6.2021
+@author     @alalloush
+---
+Description: Takes a parameter idTag and jumps to that location with smooth scrolling.
+---
+@param      idTag       HTML ID jump location     
+*/
+function SmoothScroll(idTag){
+    var element = document.getElementById(idTag);
     element.scrollIntoView({behavior: 'smooth'})
 
 }
